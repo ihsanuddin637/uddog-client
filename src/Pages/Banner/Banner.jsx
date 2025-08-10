a98d20f8a0129ed10c9b902d93b2d964afb404be
ihsanuddin637 <ihsanuddin0003@gmail.com>
2025-08-10 18:40:43 +0600

    changed section gap

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -11,20 +11,22 @@ const Banner = () => {
     >
       <div className="hero-overlay rounded-2xl"></div>
       <div className="hero-content text-neutral-content text-center">
-        <div className="max-w-md space-y-3">
-          <h1 className="text-5xl text-white font-bold">Drive Social Change Through Community Events</h1>
+        <div className="max-w-10/12 space-y-3">
+          <h1 className="text-5xl text-white font-bold">
+            Drive Social Change Through Community Events
+          </h1>
           <p className="text 2xl text-white font-medium">
             Join, create, and manage impactful events that bring people together
             and make a difference.
           </p>
           <Link to="/upcoming-event">
             <button className="relative inline-block px-4 py-2 font-medium group">
-            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-[#129ee7] group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-            <span className="absolute inset-0 w-full h-full bg-white border-2 border-[#129ee7] group-hover:bg-[#129ee7]"></span>
-            <span className="relative text-black group-hover:text-white">
-              Explore Event
-            </span>
-          </button>
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-[#129ee7] group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-[#129ee7] group-hover:bg-[#129ee7]"></span>
+              <span className="relative text-black group-hover:text-white">
+                Explore Event
+              </span>
+            </button>
           </Link>
         </div>
       </div>
--- src/Pages/GallerySection/GallerySection.jsx
+++ src/Pages/GallerySection/GallerySection.jsx
@@ -6,8 +6,8 @@ const GallerySection = () => {
       <h1 className="text-5xl font-bold text-[#129ee7] text-center pb-7">
         Gallery
       </h1>
-      <div className="flex sm:flex-row flex-col justify-center gap-5">
-        <div className="space-y-5">
+      <div className="flex sm:flex-row flex-col justify-center gap-7">
+        <div className="space-y-7">
           <div>
             <img
               className="w-[300px] mx-auto lg:w-72 h-[196px] rounded-xl"
@@ -23,7 +23,7 @@ const GallerySection = () => {
             />
           </div>
         </div>
-        <div className="space-y-5">
+        <div className="space-y-7">
           <div>
             <img
               className="w-[300px] mx-auto lg:w-72 h-96 rounded-xl"
@@ -39,7 +39,7 @@ const GallerySection = () => {
             />
           </div>
         </div>
-        <div className="space-y-5">
+        <div className="space-y-7">
           <div>
             <img
               className="w-[300px] mx-auto lg:w-72 h-[196px] rounded-xl"
@@ -55,7 +55,7 @@ const GallerySection = () => {
             />
           </div>
         </div>
-        <div className="space-y-5">
+        <div className="space-y-7">
           <div>
             <img
               className="w-[300px] mx-auto lg:w-72 h-96 rounded-xl"
